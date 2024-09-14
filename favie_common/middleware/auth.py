b16091e82e4ca1b2a2df111a8b3a75d0dbb140a4
kaka <kaka@srp.one>
2024-09-14 11:53:39 +0800

    add route level auth

--- favie_common/middleware/auth.py
+++ favie_common/middleware/auth.py
@@ -1,8 +1,16 @@
 import httpx
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import HTTPException, Request
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 
-app = FastAPI()
+
+async def get_user_info(token: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            "http://user-interface.favie.svc.cluster.local/auth/verify", headers={"Authorization": token}
+        )
+        if response.status_code == 200:
+            return response.json()
+        return None
 
 
 class AuthMiddleware(BaseHTTPMiddleware):
@@ -11,7 +19,7 @@ class AuthMiddleware(BaseHTTPMiddleware):
         if not authorization:
             raise HTTPException(status_code=401, detail="Authorization header missing")
 
-        user_info = await self.get_user_info(authorization)
+        user_info = await get_user_info(authorization)
 
         if not user_info:
             raise HTTPException(status_code=401, detail="Invalid token or user not found")
@@ -22,11 +30,18 @@ class AuthMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
         return response
 
-    async def get_user_info(self, token: str):
-        async with httpx.AsyncClient() as client:
-            response = await client.get(
-                "http://user-interface.favie.svc.cluster.local/auth/verify", headers={"Authorization": token}
-            )
-            if response.status_code == 200:
-                return response.json()
-            return None
+
+# 定义接口级别中间件
+class AuthDependency:
+    async def __call__(self, request: Request):
+        authorization: str = request.headers.get("authorization")
+        if not authorization:
+            raise HTTPException(status_code=401, detail="Authorization header missing")
+
+        user_info = await get_user_info(authorization)
+
+        if not user_info:
+            raise HTTPException(status_code=401, detail="Invalid token or user not found")
+
+        # 将用户信息放入请求头
+        request.state.user_info = user_info
